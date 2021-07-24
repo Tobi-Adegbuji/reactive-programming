58044815cc1b8d7fffd95a7718f15fe79f37682d
Tobi Adegbuji <adegbuji_tobi@yahoo.com>
2021-07-24 11:14:13 -0400

    Flux With State using a supplier and a bifunction

--- src/main/java/dev/tobiadegbuji/reactiveprogramming/flux_tutorial/Assignment3.java
+++ src/main/java/dev/tobiadegbuji/reactiveprogramming/flux_tutorial/Assignment3.java
@@ -4,12 +4,17 @@ import dev.tobiadegbuji.reactiveprogramming.utils.Utils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.SynchronousSink;
 
-import java.util.function.Consumer;
-
 public class Assignment3 {
 
+    //Generate country names until you hit Canada but using  Flux.generate or complete flux
+    //after 10 items are emitted.
 
-    public static void main(String[] args) {
+    //THIS IS ONE WAY OF USING A COUNTER
+
+
+    public static int counter = 1;
+
+    private static void main(String[] args) {
 
         Flux.generate(Assignment3::generateCountryNames)
                 .subscribe(Utils.getSubscriber(""));
@@ -18,14 +23,17 @@ public class Assignment3 {
 
     private static void generateCountryNames(SynchronousSink<Object> sink){
 
-        String countryName = "";
-        countryName = Utils.faker().country().name();
+        String countryName = Utils.faker().country().name();
 
-        sink.next(countryName);
 
-        if(countryName.equalsIgnoreCase("canada"))
+
+        if(countryName.equalsIgnoreCase("canada")  || counter > 40){
+            counter = 1;
             sink.complete();
+        }
 
+        sink.next(countryName + " - " + counter);
+        counter++;
     }
 
 }
